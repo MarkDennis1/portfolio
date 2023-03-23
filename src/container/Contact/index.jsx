3b3f725a0e62574ac22a2370217b3a384e4f4530
Mark Dennis <markdennissanchez1@gmail.com>
2023-03-24 00:28:01 +0800

    add required to input

--- src/container/Contact/index.jsx
+++ src/container/Contact/index.jsx
@@ -65,6 +65,7 @@ const Contact = () => {
                 name="user_name"
                 className="mb-4 rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="First Name Surname"
+                required
               />
             </div>
             <label
@@ -82,6 +83,7 @@ const Contact = () => {
                 name="user_email"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@gmail.com"
+                required
               />
             </div>
             <textarea
